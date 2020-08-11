30071422c0762c9d0e83c7c0d1fe2b757aa89c51
Micah Lund <micah.r.lund@gmail.com>
2020-08-11 17:41:47 -0600

    Added more content to see how the server runs.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
 const Joi = require('joi'); //used for data validation
+const bodyParser = require('body-parser');
+const port = 3005;
 
 app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/', (request, response) => {
+    response.json({Welcome: 'How to create an API with Node.js, EXPRESS, and PostgreSQL'})
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
