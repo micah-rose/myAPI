7e3d9d7665c58b369e87ff0dc9758ab1f7b0503b
Micah Lund <micah.r.lund@gmail.com>
2020-08-11 18:13:21 -0600

    Created a constant function to request data from the countries table.

--- queries.js
+++ queries.js
@@ -6,3 +6,12 @@ const pool = new Pool({
     password: 'testuser',
     port: 5432,
 })
+
+const getCountries = (request, response) => {
+    pool.query('SELECT * FROM countries ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
